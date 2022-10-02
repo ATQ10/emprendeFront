7d6de2c6107ed872289f90f30072d71f4f185266
yamileGM <yamile_gutierrez19@outlook.com>
2022-10-02 15:28:45 -0500

    Corrección de diseño, mejora de login y creación de componente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './Components/header/header.component';
 import { MenuAccessComponent } from './Components/menu-access/menu-access.component';
 import { MyPerfilComponent } from './Components/my-perfil/my-perfil.component';
 import { LoggedGuard } from 'src/service/guard/logged.guard';
+import { MyBusinessComponent } from './Components/my-business/my-business.component';
 
 const routes: Routes = [
   //{ path: "", component: AppComponent, pathMatch: "full" },
@@ -14,7 +15,8 @@ const routes: Routes = [
   { path: "register", component: RegisterComponent, canActivate:[LoggedGuard], pathMatch: "full" },
   { path: "header", component: HeaderComponent, pathMatch: "full" },
   { path: "myPerfil", component: MyPerfilComponent, pathMatch: "full" },
-  { path: "", component: MenuAccessComponent, pathMatch: "full" }
+  { path: "", component: MenuAccessComponent, pathMatch: "full" },
+  { path: "myBusiness", component: MyBusinessComponent, pathMatch: "full" }
   
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { MenuAccessComponent } from './Components/menu-access/menu-access.compon
 import { HeaderComponent } from './Components/header/header.component';
 import { AuthInterceptorService } from 'src/service/intercept/auth-interceptor.service';
 import { MyPerfilComponent } from './Components/my-perfil/my-perfil.component';
+import { MyBusinessComponent } from './Components/my-business/my-business.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { MyPerfilComponent } from './Components/my-perfil/my-perfil.component';
     LoginComponent,
     MenuAccessComponent,
     HeaderComponent,
-    MyPerfilComponent
+    MyPerfilComponent,
+    MyBusinessComponent
   ],
   imports: [
     BrowserModule,
