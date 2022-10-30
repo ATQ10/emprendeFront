f280e63cdaff6562a75e140c950c14b43f1c1d3f
MusaDeSaturno <loredana_ocampo@hotmail.com>
2022-10-29 23:57:15 -0500

    creacion de inicio

--- src/app/Components/menu-access/menu-access.component.ts
+++ src/app/Components/menu-access/menu-access.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/service/intercept/auth.service';
 
 @Component({
   selector: 'app-menu-access',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuAccessComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public authService: AuthService
+  ) { }
 
   ngOnInit(): void {
   }
